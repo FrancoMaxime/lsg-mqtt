34f99723422216058c2d0d038860e2d6b9fd9db5
Maxime Franco <franco.maxime@gmail.com>
2020-03-13 14:52:42 +0100

    ajout génération du plot et envoi server

--- lsg_mqtt.py
+++ lsg_mqtt.py
@@ -5,6 +5,7 @@ import time
 import requests
 import threading
 import serial
+import matplotlib.pyplot as plt
 
 
 BROKER_ADDRESS = "0.0.0.0"
@@ -16,9 +17,11 @@ class MyMQTTClass(mqtt.Client):
         self.capturing = False
         self.id_plateau = client_id
         self.broker_address = broker_address
-        self.serial = None
         self.filename = None
         self.capturethread = CaptureThread(self)
+        self.drink = []
+        self.meal = []
+        self.count = []
 
     def on_message(self, mqttc, obj, msg):
         data = msg.payload.decode("utf-8")
@@ -26,12 +29,24 @@ class MyMQTTClass(mqtt.Client):
         id = data[0]
         if id != self.id_plateau:
             if data[1] == "END MEAL":
+                print("Starting end...")
                 self.capturing = False
                 url = 'http://' + BROKER_ADDRESS + ':5000/tray/data'
-                myfiles = {'data': open(self.filename ,'rb')}
+                
+                print("Making plot...")
+                self.make_plot()
+                
+                print("Sending to the server...")
+                myfiles = {'data': open(self.filename ,'rb'), 'image': open(self.filename.split('.')[0]+'.png' ,'rb')}
                 x = requests.post(url, files = myfiles)
+                
                 print("END MEAL")
                 myfiles['data'].close()
+                myfiles['image'].close()
+                self.drink = []
+                self.meal = []
+                self.count = []
+                self.filename = None
             elif data[1] == "START MEAL":
                 self.capturing = True
                 self.filename = data[2]
@@ -47,6 +62,16 @@ class MyMQTTClass(mqtt.Client):
         
     def is_threading(self):
         return self.capturing
+        
+    def make_plot(self):
+        plt.figure(figsize=(20, 10))
+        plt.plot(self.count, self.drink, linewidth=0.50, label="Drink")
+        plt.plot(self.count, self.meal, linewidth=0.50, label="Meal")
+        plt.xlabel('Time')
+        plt.ylabel('Weight')
+        plt.title("Meal N°" + self.filename.split('.')[0])
+        plt.legend()
+        plt.savefig(self.filename.split('.')[0]+'.png')
 
 
 class CaptureThread(threading.Thread):
@@ -71,9 +96,20 @@ class CaptureThread(threading.Thread):
                 with open(self.config.filename , "a+")as f:
                     try:
                         x = self.ser.readline().decode('utf-8')
-                        f.write(x)
+                        f.write("floup")
+                        x = x.split("\t")
+                        self.config.drink.append(float(x[1]))
+                        self.config.meal.append(float(x[4]))
+                        if len(self.config.count) == 0:
+                            self.config.count.append(0.0)
+                        else:
+                            self.config.count.append(self.config.count[-1] + 1)
+                            
                     finally:
                         time.sleep(1)
+            else:
+                self.ser = None
+                time.sleep(1)
                 
 
 client = MyMQTTClass(ID_TRAY, BROKER_ADDRESS)
